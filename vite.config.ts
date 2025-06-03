d53c4950fac6b833320198df5ff3eab3e1209f99
Working-aanas <workinganass@gmail.com>
2025-06-03 01:15:14 +0000

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,22 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/futuristic_portfolio_website_fs35ii/',  // Adjust this line to match your repository name
-  plugins: [
-    react(),
-  ],
+  base: '/futuristic_portfolio_website_fs35ii/',
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
+    }
   },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      external: [],
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom']
+        }
+      }
+    }
+  }
 }));
