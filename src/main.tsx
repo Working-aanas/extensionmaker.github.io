000b68aac12dfc319bc59f5891cc09a132d5c6ab
Working-aanas <workinganass@gmail.com>
2025-06-03 01:10:01 +0000

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import '../src/index.css';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
